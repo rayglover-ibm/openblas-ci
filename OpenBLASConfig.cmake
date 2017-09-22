cd055562120778617479b6115a7d79b48d7bcd0b
Ray Glover <gh@rayglover.net>
2017-09-22 23:16:31 +0100

    custom openblas targets

--- OpenBLASConfig.cmake
+++ OpenBLASConfig.cmake
@@ -1,49 +1,60 @@
 cmake_minimum_required (VERSION 2.8)
 
-set (pkgname "OpenBLAS")
-add_library (${pkgname} SHARED IMPORTED GLOBAL)
-
 # options ---------------------------------------------------------------------
 set ("OpenBLAS_microarch" "NEHALEM" CACHE STRING "CPU microarchitecture to use")
 # -----------------------------------------------------------------------------
 
-if (CMAKE_SIZEOF_VOID_P MATCHES "8")
-    set (arch "x86-64")
-else ()
-    set (arch "x86")
-endif ()
+set_property (GLOBAL PROPERTY OpenBLAS_ld "${CMAKE_CURRENT_LIST_DIR}")
 
-if (${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Windows")
-    set (platform  "windows")
-    set (toolchain "mingw")
-    set (libext    "dll")
-    set (libdir    "bin")
+function (OpenBLAS_import)
+    set (oneValueArgs TARGET MICRO_ARCH)
+    cmake_parse_arguments (args "" "${oneValueArgs}" "" ${ARGN})
 
-elseif (${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Linux")
-    set (platform  "linux")
-    set (toolchain "gcc")
-    set (libext    "so")
-    set (libdir    "lib")
+    if ("${args_MICRO_ARCH}" STREQUAL "")
+        set (args_MICRO_ARCH "${OpenBLAS_microarch}")
+    endif ()
 
-elseif (${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Darwin")
-    set (platform  "darwin")
-    set (toolchain "gcc")
-    set (libext    "dylib")
-    set (libdir    "lib")
+    if (CMAKE_SIZEOF_VOID_P MATCHES "8")
+        set (arch "x86-64")
+    else ()
+        set (arch "x86")
+    endif ()
 
-else ()
-    message (SEND_ERROR "These builds of ${pkgname} dont support ${CMAKE_HOST_SYSTEM_NAME}")
-endif ()
+    set (pkgname "OpenBLAS")
+    add_library (${args_TARGET} SHARED IMPORTED GLOBAL)
 
-get_filename_component (blasbase
-    "${CMAKE_CURRENT_LIST_DIR}/${platform}/${toolchain}/${OpenBLAS_microarch}/${arch}" ABSOLUTE
-)
-set_target_properties (${pkgname} PROPERTIES
-    IMPORTED_IMPLIB               "${blasbase}/lib/libopenblas.${libext}.a"
-    IMPORTED_LOCATION             "${blasbase}/${libdir}/libopenblas.${libext}"
-    INTERFACE_INCLUDE_DIRECTORIES "${blasbase}/include"
-)
+    if (${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Windows")
+        set (platform  "windows")
+        set (toolchain "mingw")
+        set (libext    "dll")
+        set (libdir    "bin")
 
-message (STATUS
-    "Using ${pkgname} from: ${blasbase}"
-)
+    elseif (${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Linux")
+        set (platform  "linux")
+        set (toolchain "gcc")
+        set (libext    "so")
+        set (libdir    "lib")
+
+    elseif (${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Darwin")
+        set (platform  "darwin")
+        set (toolchain "gcc")
+        set (libext    "dylib")
+        set (libdir    "lib")
+
+    else ()
+        message (SEND_ERROR "These builds of ${pkgname} dont support ${CMAKE_HOST_SYSTEM_NAME}")
+    endif ()
+
+    get_property (ld GLOBAL PROPERTY OpenBLAS_ld)
+    get_filename_component (blasbase
+        "${ld}/${platform}/${toolchain}/${args_MICRO_ARCH}/${arch}" ABSOLUTE
+    )
+    set_target_properties (${args_TARGET} PROPERTIES
+        IMPORTED_IMPLIB               "${blasbase}/lib/libopenblas.${libext}.a"
+        IMPORTED_LOCATION             "${blasbase}/${libdir}/libopenblas.${libext}"
+        INTERFACE_INCLUDE_DIRECTORIES "${blasbase}/include"
+    )
+    message (STATUS
+        "Importing ${pkgname} from: ${blasbase}"
+    )
+endfunction ()
