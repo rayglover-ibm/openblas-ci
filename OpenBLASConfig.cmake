046444f4bd78d8337c9d8dfbf0dd39a206301b08
Ray Glover <gh@rayglover.net>
2017-09-23 22:09:06 +0100

    Use cmake components [skip ci]

--- OpenBLASConfig.cmake
+++ OpenBLASConfig.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required (VERSION 2.8)
+cmake_minimum_required (VERSION 3.2)
 
 # options ---------------------------------------------------------------------
 set ("OpenBLAS_microarch" "NEHALEM" CACHE STRING "CPU microarchitecture to use")
@@ -7,21 +7,14 @@ set ("OpenBLAS_microarch" "NEHALEM" CACHE STRING "CPU microarchitecture to use")
 set_property (GLOBAL PROPERTY OpenBLAS_ld "${CMAKE_CURRENT_LIST_DIR}")
 
 function (OpenBLAS_import)
-    set (oneValueArgs TARGET MICRO_ARCH)
+    set (oneValueArgs MICRO_ARCH)
     cmake_parse_arguments (args "" "${oneValueArgs}" "" ${ARGN})
-
-    if ("${args_MICRO_ARCH}" STREQUAL "")
-        set (args_MICRO_ARCH "${OpenBLAS_microarch}")
-    endif ()
 
     if (CMAKE_SIZEOF_VOID_P MATCHES "8")
         set (arch "x86-64")
     else ()
         set (arch "x86")
     endif ()
-
-    set (pkgname "OpenBLAS")
-    add_library (${args_TARGET} SHARED IMPORTED GLOBAL)
 
     if (${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Windows")
         set (platform  "windows")
@@ -49,12 +42,27 @@ function (OpenBLAS_import)
     get_filename_component (blasbase
         "${ld}/${platform}/${toolchain}/${args_MICRO_ARCH}/${arch}" ABSOLUTE
     )
-    set_target_properties (${args_TARGET} PROPERTIES
-        IMPORTED_IMPLIB               "${blasbase}/lib/libopenblas.${libext}.a"
-        IMPORTED_LOCATION             "${blasbase}/${libdir}/libopenblas.${libext}"
-        INTERFACE_INCLUDE_DIRECTORIES "${blasbase}/include"
-    )
-    message (STATUS
-        "Importing ${pkgname} from: ${blasbase}"
-    )
+    
+    set (tgt "OpenBLAS::${args_MICRO_ARCH}")
+    if (NOT TARGET ${tgt})
+        message (STATUS
+            "[OpenBLAS] Importing '${tgt}' from: ${blasbase}"
+        )
+        add_library ("${tgt}" SHARED IMPORTED GLOBAL)
+        set_target_properties (${tgt} PROPERTIES
+            IMPORTED_IMPLIB               "${blasbase}/lib/libopenblas.${libext}.a"
+            IMPORTED_LOCATION             "${blasbase}/${libdir}/libopenblas.${libext}"
+            INTERFACE_INCLUDE_DIRECTORIES "${blasbase}/include"
+        )
+    endif ()
 endfunction ()
+
+if (OpenBLAS_FIND_COMPONENTS)
+    # create targets. Each component corresponds to a specific micro-architecture
+    foreach (microarch ${OpenBLAS_FIND_COMPONENTS})
+        OpenBLAS_import (MICRO_ARCH "${microarch}")
+    endforeach ()
+else ()
+    # use the option OpenBLAS_microarch
+    OpenBLAS_import (MICRO_ARCH "${OpenBLAS_microarch}")
+endif ()
